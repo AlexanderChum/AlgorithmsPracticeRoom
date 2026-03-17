91317d0c89690767600426352d41028ff5830a22
AlexanderChum <Senneiko@yandex.ru>
2026-03-18 01:45:25 +0300

    Solved first and preset class for second task in first contest

---
+++ src/Contest1B.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Contest1B {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+
+
+        reader.close();
+    }
+}
